4a6c94afecea88a306653631cf02a3802e5ec31b
Keydra Burtin <bkeydra@gmail.com>
2023-05-07 15:34:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+
+const YOUR_NAME = "Mohammad Elyas";
+newHeader.innerHTML = `${YOUR_NAME} is the champion`
